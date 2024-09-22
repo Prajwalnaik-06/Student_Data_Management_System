a8117be3e87bfa3f63ddbf8100c05a79c54988a4
Prajwalnaik-06 <prajwalnk2003@gmail.com>
2024-09-22 11:30:31 +0530

    first commit

---
+++ studentsystem/src/main/java/com/sample/studentsystem/controller/StudentController.java
@@ -0,0 +1,30 @@
+package com.sample.studentsystem.controller;
+
+import com.sample.studentsystem.model.Student;
+import com.sample.studentsystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/student")
+
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New Student is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+
+        return studentService.getAllStudents();
+
+    }
+}
